ae770fb9cb93cdd411b1c4ed5a4300c6bd19c33a
Saranphon Phaithoon <Saranphon.Phaithoon@ibm.com>
2022-09-09 21:57:44 +0700

    update script to preserve file permission

--- script/symmetric-encrypt.sh
+++ script/symmetric-encrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ True ]; do
-if [ "$1" = "--passphase" -o "$1" = "-ps" ]; then
+if [ "$1" = "--passphrase" -o "$1" = "-ps" ]; then
     PASSPHRASE=$2
     FOLDER_PATH=$3
     DESTINATION_SHARE_PATH=$4
@@ -16,22 +16,22 @@ for i in ${ARG[@]}; do
   echo $i
 done
 
-GPG_TTY=$(tty)
-export GPG_TTY
-
 echo "Path to folder for encrypting file: $FOLDER_PATH"
 echo "Path to folder to copy to SMB share: $DESTINATION_SHARE_PATH"
 
 encrypt() {
   find "$FOLDER_PATH" -type f -print0 | while IFS= read -r -d $'\0' FILE; 
   do echo "[Encrypting] $FILE";
-    gpg --symmetric --cipher-algo AES256 --passphrase "$PASSPHRASE" --pinentry-mode loopback $FILE
+    gpg --symmetric --cipher-algo AES256 --passphrase "$PASSPHRASE" --batch $FILE
   done
 }
 
 copy_gpg_to_destination () {
-  find "$FOLDER_PATH"/ -name '*.gpg' -exec rsync -R {} "$DESTINATION_SHARE_PATH" \;
+  echo "Copy to destination path $DESTINATION_SHARE_PATH"
+  find "$FOLDER_PATH"/ -name '*.gpg' -exec rsync -R -a {} "$DESTINATION_SHARE_PATH" \;
 }
 
+## on-prem Red Hat 
+## on-prem GNUPG 2.0.22
 encrypt
 copy_gpg_to_destination
